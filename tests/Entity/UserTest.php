66c18769d025bc6dc07e65588506f9ad9961da0e
benjamin.pelletier <pelletierbenjamin90@gmail.com>
2019-09-24 11:52:25 +0200

    Initial commit tests App

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    private $objectManager;
+
+    public function __construct(ObjectManager $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    public function testNameUser()
+    {
+
+    }
+
+    public function testCreateSameTrick()
+    {
+        $userRepository = $this->objectManager->getRepository(User::class);
+
+    }
+}
