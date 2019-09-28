41d7f551fb90029dd75e540052e7acabab7778e7
bpel <pelletierbenjamin90@gmail.com>
2019-09-28 19:54:32 +0200

    Update Mail functionality

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -2,15 +2,29 @@
 
 namespace App\Service;
 
+use Swift_Attachment;
+use Twig\Environment;
+
 class Mailer {
 
     private $mailer;
     private $from = "no-reply@example.fr";
     private $reply = "contact@example.fr";
+    private $twig;
 
-    public function __construct(\Swift_Mailer $mailer)
+    public function __construct(\Swift_Mailer $mailer, Environment $twig)
     {
         $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
+    public function getDate()
+    {
+        try {
+            return new \DateTime('now');
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 
     public function sendMessage($to, $subject, $bodyMessage)
@@ -19,11 +33,14 @@ class Mailer {
             ->setFrom($this->from)
             ->setReplyTo($this->reply)
             ->setTo($to)
-            ->setBody($bodyMessage)
+            ->setBody(
+                $this->twig->render('mail/mail.html.twig', [
+                    'dateNow' => $this->getDate()
+                ])
+            )
             ->setContentType('text/html')
         ;
         $this->mailer->send($message);
-        return true;
     }
 
 
